abddf1cf06a221ba050054cbcbe07bfcb04907cf
lokeshsai128 <lokeshsai128@gmail.com>
2024-06-06 15:50:05 +0530

    First Message

---
+++ src/FirstPack/FirstCode.java
@@ -0,0 +1,10 @@
+package FirstPack;
+
+public class FirstCode {
+
+	public static void main(String[] args) {
+		System.out.println("This is my first code to push into github from eclipse");
+
+	}
+
+}
